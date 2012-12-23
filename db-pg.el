550e1eeec2f7627266ac8ae1734e596f6f61f303
Nic Ferier <nic@ferrier.me.uk>
2012-12-23 14:01:39 +0000

    spurious whitespace

--- db-pg.el
+++ db-pg.el
@@ -7,7 +7,6 @@
 ;; Created: 23 December 2012
 ;; Package-Requires: ((pg "0.12")(db "0.0.2"))
 
-
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
